1128b88b4b3c97a9d22061ca9cfd06144a32156e
DrunkFlamingo <matte901@gmail.com>
2018-07-21 13:44:53 -0400

    removed extra line

--- src/lua/mod.rs
+++ src/lua/mod.rs
@@ -9,7 +9,6 @@ use self::rlua::{Function, Lua, Result};
 pub fn start_lua() -> Result<()> {
     let lua = Lua::new();
     let lua_globals = lua.globals();
-    lua_globals.set("startup_string", "hello world from lua")?;
     let print: Function = lua_globals.get("print")?;
     print.call::<_, ()>("Hello World From Lua")?;
     Ok(())
