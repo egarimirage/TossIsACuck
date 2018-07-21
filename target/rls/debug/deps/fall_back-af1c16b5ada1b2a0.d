8beac2edc066db7a9b9b69a0d500235af357acb0
DrunkFlamingo <matte901@gmail.com>
2018-07-21 12:55:25 -0400

    added gitignore

--- target/rls/debug/deps/fall_back-af1c16b5ada1b2a0.d
+++ target/rls/debug/deps/fall_back-af1c16b5ada1b2a0.d
@@ -1,5 +1,5 @@
-d:\Active Projects\fall_back\fall_back\target\rls\debug\deps\fall_back-af1c16b5ada1b2a0.rmeta: src\main.rs
+d:\Active Projects\fall_back\target\rls\debug\deps\fall_back-af1c16b5ada1b2a0.rmeta: src\main.rs
 
-d:\Active Projects\fall_back\fall_back\target\rls\debug\deps\fall_back-af1c16b5ada1b2a0.d: src\main.rs
+d:\Active Projects\fall_back\target\rls\debug\deps\fall_back-af1c16b5ada1b2a0.d: src\main.rs
 
 src\main.rs:
