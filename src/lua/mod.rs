b0b61030ddc3d9abd2f556190e84137866dc43a3
DrunkFlamingo <matte901@gmail.com>
2018-07-21 13:43:38 -0400

    comments

--- src/lua/mod.rs
+++ src/lua/mod.rs
@@ -1,8 +1,11 @@
 extern crate rlua;
 
+
+// will eventually need the added options MetaMethod, UserData, UserDataMethods, Variadic but those will jsut be unused imports right now.
 use self::rlua::{Function, Lua, Result};
 
-#[allow(dead_code)]
+
+#[allow(dead_code)] // leaving this here for when we aren't using lua.
 pub fn start_lua() -> Result<()> {
     let lua = Lua::new();
     let lua_globals = lua.globals();
