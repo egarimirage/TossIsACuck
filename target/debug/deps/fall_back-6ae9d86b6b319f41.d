b35d466ddde105e805b3ef904a7e6bfa1ce37b0c
DrunkFlamingo <matte901@gmail.com>
2018-07-21 12:48:32 -0400

    built again

---
+++ target/debug/deps/fall_back-6ae9d86b6b319f41.d
@@ -0,0 +1,5 @@
+D:\Active Projects\fall_back\target\debug\deps\fall_back-6ae9d86b6b319f41.exe: src\main.rs
+
+D:\Active Projects\fall_back\target\debug\deps\fall_back-6ae9d86b6b319f41.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/fall_back.d
@@ -0,0 +1,1 @@
+D:\Active\ Projects\fall_back\target\debug\fall_back.pdb: D:\Active\ Projects\fall_back\src\main.rs
